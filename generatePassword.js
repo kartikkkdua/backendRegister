3c48540f5eae2644a78ab98a9a1da4ebc0e36288
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2024-08-26 09:15:07 +0530

    chore: Update generatePassword.js to import crypto and add default value for keySecret

--- generatePassword.js
+++ generatePassword.js
@@ -1,6 +1,4 @@
-export const generateHexCode = (input, keySecret) => {
-  return crypto
-    .createHmac('sha256', keySecret) 
-    .update(input) 
-    .digest('hex'); 
-}
+import crypto from 'crypto';
+export const generateHexCode = (input, keySecret = "default_secret") => {
+  return crypto.createHmac("sha256", keySecret).update(input).digest("hex");
+};
