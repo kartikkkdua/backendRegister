4e52dc8497b41fa9ba7b7ab31b29b84839494ae8
diptanshu1044 <diptanshubanerjee123@gmail.com>
2024-08-27 23:48:22 +0530

    coupon route added

--- model/coupon.model.js
+++ model/coupon.model.js
@@ -1,10 +1,35 @@
 import { Schema, model } from "mongoose";
 
+const generateUniqueCouponCode = async () => {
+  const min = 1000000000000000;
+  const max = 9999999999999999; 
+
+  let couponCode;
+  let existingCoupon;
+
+  do {
+    couponCode = Math.floor(Math.random() * (max - min + 1)) + min;
+    existingCoupon = await Coupon.findOne({ couponCode: couponCode.toString() });
+  } while (existingCoupon);
+
+  return couponCode.toString();
+};
+
 const couponSchema = new Schema({
+  name: {
+    type: String,
+  },
+  sapId: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+  },
   couponCode: {
     type: String,
-    required: true,
-    unique: true,
   },
   redeemed: {
     type: Boolean,
@@ -14,4 +39,17 @@ const couponSchema = new Schema({
   primeId: {
     type: String,
   },
+}, {
+  timestamps: true,
 });
+
+couponSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    this.couponCode = await generateUniqueCouponCode();
+    console.log(this.couponCode);
+  }
+  next();
+});
+
+
+export const Coupon = model("Coupon", couponSchema);
--- model/primeId.model.js
+++ model/primeId.model.js
@@ -1,7 +1,9 @@
 import { Schema, model } from 'mongoose';
 
 const primeIdSchema = new Schema({
-  PrimeIds: [String],
+  primeId: {
+    type: String,
+  },
 });
 
 export const PrimeId = model('PrimeId', primeIdSchema);
